078599145f469b943e962347e907047a877d5382
tuckervlittle <tucker.little95@gmail.com>
2025-03-06 19:48:34 -1000

    Completed week01 exercise03

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string response;
+        do
+        {
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 101);
+            int guess;
+            int tries = 0;
+
+            do
+            {
+                Console.Write("What is your guess? ");
+                string input = Console.ReadLine();
+                guess = int.Parse(input);
+                if (guess > 100)
+                {
+                    Console.WriteLine("The number is between 0 and 100. That guess won't be counted.");
+                    tries -= 1;
+                }
+                else if (guess < 0)
+                {
+                    Console.WriteLine("The number is between 0 and 100. That guess won't be counted.");
+                    tries -= 1;
+                }
+                else if (guess > number)
+                {
+                    Console.WriteLine("Your guess was too high.");
+                }
+                else if (guess < number)
+                {
+                    Console.WriteLine("Your guess was too low.");
+                }
+                tries ++;
+            } while (guess != number);
+             Console.WriteLine($"Congrats, You guessed correctly! It took you {tries} tries!");
+             Console.Write("Do you want to continue? ");
+             response = Console.ReadLine();
+        } while (response == "yes");
     }
 }
