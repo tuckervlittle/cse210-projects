26ebe0367b02379e35f1a17d53c9304d6b412d7a
tuckervlittle <tucker.little95@gmail.com>
2025-03-10 12:03:28 -1000

    Completed week02 Resume but with error?

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
+        Resume resume1 = new Resume();
+        resume1._name = "Allison Rose";
+
         Job job1 = new Job();
         job1._company = "Microsoft";
         job1._jobTitle = "Software Engineer";
@@ -15,5 +18,10 @@ class Program
         job2._jobTitle = "Manager";
         job2._startYear = 2022;
         job2._endYear = 2023;
+
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.Display();
     }
 }
